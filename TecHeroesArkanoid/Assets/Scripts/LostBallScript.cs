9f12e306d273c0e4eb0ab56fee524fc2ba8f44ac
Leonardo Turchi <info@spaceclick.com>
2016-06-13 21:25:59 +0200

    MS Dev Commit 1

---
+++ TecHeroesArkanoid/Assets/Scripts/LostBallScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LostBallScript : MonoBehaviour {
+
+    //evento che viene scatenato dal Box Collider impostato come trigger 
+    //quando un altro collider entra nella zona di collisione 
+    void OnTriggerEnter(Collider coll)
+    {
+        //se il collider è quello dell'oggetto Ball ricomincio 
+        //(più avanti nel laboratorio questa parte verrà modificata) 
+        if (coll.gameObject.name == "Ball")
+            GameManagerScript.singleton.LoseBall();
+    }
+
+}
---
+++ TecHeroesArkanoid/Assets/Scripts/MenuManagerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManagerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //PLAY
+    public void Play() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+    }
+}
